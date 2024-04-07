38cafebbb10e3f462d328781cdc24ef154c5d455
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-04-07 16:41:25 +0530

    add

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotEnv = require("dotenv");
 const employeeRoutes = require("./routes/employeeRoutes");
+const cors = require('cors')
+
+app.use(cors());
 
 const port = process.env.PORT || 7000;
 
@@ -11,13 +14,7 @@ app.use(bodyParser.json());
 
 dotEnv.config();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Allow access from any origin
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS'); // Allow these methods
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Allow these headers
-  next();
-});
-
+ 
 mongoose
   .connect(process.env.MONGO_DB)
   .then(() => {
