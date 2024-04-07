89b2c59090501a2ba31578bdf9d55cff44a1665d
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-04-07 15:50:52 +0530

    add

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.use(bodyParser.json());
 
 dotEnv.config();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow access from any origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS'); // Allow these methods
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Allow these headers
+  next();
+});
+
 mongoose
   .connect(process.env.MONGO_DB)
   .then(() => {
