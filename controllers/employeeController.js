1be0a4eab5d0045a70c275103a35811299292bf3
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-04-10 16:20:50 +0530

    add

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -64,35 +64,35 @@ const getEmployees = async (req, res) => {
 };
 
 
-const deleteUser = async (req, res) => {
-  try {
-    // Extracting the 'text' field from the request body
-    const { text } = req.body;
+// const deleteUser = async (req, res) => {
+//   try {
+//     // Extracting the 'text' field from the request body
+//     const { text } = req.body;
     
-    // Checking if the 'text' field is provided
-    if (!text) {
-      return res.status(400).json({ message: "text unable to delete." });
-    }
+//     // Checking if the 'text' field is provided
+//     if (!text) {
+//       return res.status(400).json({ message: "text unable to delete." });
+//     }
     
-    // Deleting all existing users in the database
-    await User.deleteMany({});
+//     // Deleting all existing users in the database
+//     await User.deleteMany({});
     
-    // Creating a new user object with the provided text
-    const user = new User({
-      text,
-    });
+//     // Creating a new user object with the provided text
+//     const user = new User({
+//       text,
+//     });
 
-    // Saving the user object to the database
-    await user.save();
+//     // Saving the user object to the database
+//     await user.save();
     
-    // Sending a response with the created user object
-    res.status(200).json(user);
-  } catch (error) {
-    // Handling errors
-    console.error(error);
-    res.status(500).json({ message: error.message });
-  }
-};
+//     // Sending a response with the created user object
+//     res.status(200).json(user);
+//   } catch (error) {
+//     // Handling errors
+//     console.error(error);
+//     res.status(500).json({ message: error.message });
+//   }
+// };
 
 
 module.exports = { createEmployee, getEmployees, createUser, getUser };
