d55cdce7afa4a15d8ab7b6d36a49eba1db7547f4
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-04-11 15:04:35 +0530

    add

--- models/Employee.js
+++ models/Employee.js
@@ -16,12 +16,18 @@ const employeeSchema = new mongoose.Schema({
 });
 
 const userSchema = new mongoose.Schema({
+  _id: {
+    type: Number, // or whichever type you want for your custom ID
+    required: true, // or false depending on whether you want the ID to be required
+    unique: true // if you want the custom ID to be unique
+  },
   text: {
     type: String,
     required: true,
   }
 },{
-  timestamps : true
+  timestamps : true,
+  
 });
 
 const Employee = mongoose.model("Employee", employeeSchema);
