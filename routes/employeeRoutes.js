b4f28fe619b34c99dded18f0b6896eaf10bb5682
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-04-23 17:51:00 +0530

    add

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -1,23 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const employeeController = require("../controllers/employeeController");
-const multer = require("multer")
-// image multer code 
+const multer = require("multer");
+// image multer code
 
 const upload = multer({
-    limits: {
-      fileSize: 5 * 1024 * 1024, // 5 MB file size limit
-    },
-    fileFilter: (req, file, cb) => {
-      if (file.mimetype.startsWith('image/')) {
-        cb(null, true);
-      } else {
-        cb(new Error('Please upload only images'));
-      }
-    },
-  });
-
-
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5 MB file size limit
+  },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Please upload only images"));
+    }
+  },
+});
 
 // POST , GET , PUT , DELETE route for employeeS
 router.post("/add-emp", employeeController.createEmployee);
@@ -31,8 +29,6 @@ router.get("/getuserbyid/:id", employeeController.getUserById);
 router.put("/updateuserbyid/:id", employeeController.updateUserById);
 router.post("/getempbyphone", employeeController.findByOneEmp);
 router.put("/updatebyidemp/:id", employeeController.updateByIdEmp);
-router.post("/imgPost", upload.single('image'),employeeController.uploadImage)
-
-
+router.post("/upload", upload.single("image"), employeeController.uploadImage);
 
 module.exports = router;
